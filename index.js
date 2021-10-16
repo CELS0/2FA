acf68c61369ea61cf7e9d61557c6672c1d2ad991
CELS0 <celsojr.300@gmail.com>
2021-10-16 17:53:53 -0300

    feat: add validated

--- index.js
+++ index.js
@@ -60,6 +60,34 @@ app.post('/api/verify', (req, res) => {
     }
 })
 
+app.post('/api/validated', (req, res) => {
+    const { token, userId } = req.body;
+
+    try {
+        const path = `/user/${userId}`;
+        const user = db.getData(path);
+
+        const { base32: secret } = user.secret;
+
+        const tokenValidates = speakeasy.totp.verify({
+            secret,
+            encoding: 'base32',
+            token,
+            window: 1,
+        })
+
+        if (tokenValidates) {
+            res.json({ validated: true })
+        }else{
+            res.json({ validated: false })
+        }
+    }
+    catch (err) { 
+        console.log(err);
+        res.status(500).json({ message: `Error registering user ${err}` });
+    }
+})
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
